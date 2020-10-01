f1ed18442ae72441dabb56c191b12b4ca9290bd5
Michael Creel <michael.creel@uab.es>
2020-10-01 16:41:02 +0200

    begin update for julia 1.x

--- example1.jl
+++ example1.jl
@@ -1,22 +1,29 @@
-import MPI
+using Pkg
+Pkg.activate("./")
+using MPI, Statistics
 function main()
-    MPI.Init()
+    if !MPI.Initialized()
+        MPI.Init()
+    end    
     comm = MPI.COMM_WORLD
     size = MPI.Comm_size(comm)
     rank = MPI.Comm_rank(comm)
     recv_mesg = zeros(100,1)
     if rank == 0
-        send_mesg = rand(100,1)
         for i = 1:size-1
-            MPI.Send(send_mesg, i, 0, comm)
+            MPI.Send(rand(100), i, 0, comm)
         end
     else
         MPI.Recv!(recv_mesg, 0, 0, comm)
     end    
-    m = mean(recv_mesg,1)
-    sleep(rank*2)
-    println("Results on rank ",rank,": ",m)
     MPI.Barrier(comm)
+    m = mean(recv_mesg,dims=1)
+    for i=1:size-1
+        if rank==i
+            println("Results on rank ",i,": ",m)
+        end
+        sleep(rank) # print out results in rank order
+    end    
     MPI.Finalize()
 end
 main()
